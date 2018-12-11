b73929ad906b5b256303a426f6191f2a05f2a177
0dsabella <42749703+0dsabella@users.noreply.github.com>
2018-12-11 13:54:52 -0600

    Add files via upload

--- securepass.js
+++ securepass.js
@@ -20,10 +20,9 @@ function upperCaseCheck(password){
   
   for(let i = 0; i < password.length; i++){
 
-    if(password.fromCharCode(i) >= String.fromCharCode(65-90)){
-      return true;
+    if(password.CharCodeAt(i) >= 65 && password.CharCodeAt(i) <= 90){
+      return true
     }
-
   }
   return false;
 }
@@ -36,7 +35,15 @@ function upperCaseCheck(password){
 //returns true or false if there is an lowercase character in the password
 function lowerCaseCheck(password){
 
+  for(let i = 0; i < password.length; i++){
 
+    if(password.CharCodeAt(i) >= String.fromCharCode(97) && (password.CharCodeAt(i) <= String.fromCharCode(122))){
+      return true;
+    }
+    else if(password.CharCodeAt(i) < String.fromCharCode(97) || (password.CharCodeAt(i) > String.fromCharCode(122))){
+      return false;
+    }
+  } 
 }
 
 
@@ -49,7 +56,15 @@ function lowerCaseCheck(password){
 //returns true or false if there is a numerical character
 function numberCheck(password){
 
+  for(let i = 0; i < password.length; i++){
 
+    if(password.CharCodeAt(i) >= String.fromCharCode(48) && (password.CharCodeAt(i) <= String.fromCharCode(57))){
+      return true
+    }
+    else if(password.CharCodeAt(i) < String.fromCharCode(48) || (password.CharCodeAt(i) > String.fromCharCode(57))){
+      return false;
+    }
+  } 
 }
 
 
@@ -61,7 +76,24 @@ function numberCheck(password){
 //returns true or false if there is a special character in the password
 function specialCheck(check){
 
+  for(let i = 0; i < password.length; i++){
 
+    if(password.CharCodeAt(i) >= String.fromCharCode(33) && (password.CharCodeAt(i) <= String.fromCharCode(47))){
+      return true
+    }
+    if(password.CharCodeAt(i) >= String.fromCharCode(58) && (password.CharCodeAt(i) <= String.fromCharCode(64))){
+      return true
+    }
+    if(password.CharCodeAt(i) >= String.fromCharCode(91) && (password.CharCodeAt(i) <= String.fromCharCode(96))){
+      return true
+    }
+    if(password.CharCodeAt(i) >= String.fromCharCode(125) && (password.CharCodeAt(i) <= String.fromCharCode(127))){
+      return true
+    }
+    else if(password.CharCodeAt(i) < String.fromCharCode(65) || (password.CharCodeAt(i) > String.fromCharCode(90))){
+      return false;
+    }
+  } 
 }
 
 
